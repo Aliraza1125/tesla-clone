de3ecbd4d41c3ccbf65578d948390a76399e6446
Ali Raza <alirazaasif2511@gmail.com>
2024-10-11 01:46:48 +0600

    updated

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Fade from 'react-reveal/Fade'
+import { CSSTransition } from 'react-transition-group';
+
 const Section = ({
   title,
   description,
@@ -8,22 +9,28 @@ const Section = ({
   leftBtnText,
   rightBtnText,
 }) => {
+  const [inProp, setInProp] = useState(false);
+
+  useEffect(() => {
+    setInProp(true);
+  }, []);
+
   return (
     <Wrap bgImage={backgroundImg}>
-      <Fade bottom>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
-      </Fade>
+      <CSSTransition in={inProp} timeout={1000} classNames="fade">
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </CSSTransition>
       
       <Buttons>
-      <Fade bottom>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
-        </ButtonGroup>
-        </Fade>
+        <CSSTransition in={inProp} timeout={1000} classNames="fade">
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+        </CSSTransition>
         <DownArrow src={"/images/down-arrow.svg"} />
       </Buttons>
     </Wrap>
@@ -40,25 +47,47 @@ const Wrap = styled.div`
   background-repeat: no-repeat;
   display: flex;
   flex-direction: column;
-  justify-content: space-between; //vertial
+  justify-content: space-between;
   align-items: center;
   background-image: ${(props) => `url("/images/${props.bgImage}")`};
-  scroll-snap-align:start;
-  z-index:1;
+  scroll-snap-align: start;
+  z-index: 1;
 `;
 
 const ItemText = styled.div`
   padding-top: 15vh;
   text-align: center;
+  
+  &.fade-enter {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  &.fade-enter-active {
+    opacity: 1;
+    transform: translateY(0);
+    transition: opacity 1000ms, transform 1000ms;
+  }
 `;
 
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+  
+  &.fade-enter {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  &.fade-enter-active {
+    opacity: 1;
+    transform: translateY(0);
+    transition: opacity 1000ms, transform 1000ms;
+  }
+  
   @media (max-width: 768px) {
     flex-direction: column;
   }
 `;
+
 const LeftButton = styled.div`
   background-color: rgba(23, 26, 32, 0.8);
   height: 40px;
@@ -74,6 +103,7 @@ const LeftButton = styled.div`
   cursor: pointer;
   margin: 8px;
 `;
+
 const RightButton = styled(LeftButton)`
   background: white;
   opacity: 0.65;
@@ -85,4 +115,5 @@ const DownArrow = styled.img`
   overflow-x: hidden;
   animation: animateDown infinite 1.5s;
 `;
+
 const Buttons = styled.div``;
